debd872e820a9c90a254fc1dc8718d93b546ed6d
enisn <info@enisnecipoglu.com>
2024-11-14 16:56:58 +0300

    Update install-winget.ps1

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,5 +1,6 @@
 $progressPreference = 'silentlyContinue'
 Write-Host "Installing WinGet PowerShell module from PSGallery..."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
